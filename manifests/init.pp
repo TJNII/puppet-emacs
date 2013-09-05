4285a63d1ae2e75fc4b1e463bfa47e2c7433cec3
Tom Noonan II <root@tjnii.com>
2013-09-05 14:18:54 -0500

    Added RedHat support

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,16 +1,37 @@
 class emacs {
-  package { ['emacs23-nox', 'emacsen-common']:
-    ensure => installed,
+  case $operatingsystem {
+    debian, ubuntu: {
+      package { ['emacs23-nox', 'emacsen-common']:
+        ensure => installed,
+      }
+
+      file { "/etc/emacs/site-start.d/70nobackups.el":
+        ensure  => file,
+        owner   => root,
+        group   => root,
+        mode    => 644,
+        require => Package['emacsen-common'],
+        source  => "puppet:///modules/emacs/70nobackups.el",
+      }
+      
+    }
+    centos, redhat: {
+      package { ['emacs-nox', 'emacs-common']:
+        ensure => installed,
+      }
+
+      file { "/usr/share/emacs/site-lisp/site-start.d/nobackups.el":
+        ensure  => file,
+        owner   => root,
+        group   => root,
+        mode    => 644,
+        require => Package['emacs-common'],
+        source  => "puppet:///modules/emacs/70nobackups.el",
+      }
+      
+    }
   }
   
-  file { "/etc/emacs/site-start.d/70nobackups.el":
-    ensure  => file,
-    owner   => root,
-    group   => root,
-    mode    => 644,
-    require => Package['emacsen-common'],
-    source  => "puppet:///modules/emacs/70nobackups.el",
-  }
 }
               
                                    
