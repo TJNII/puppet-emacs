fbd5c3c51eb72e495cd588de1785d6f8955d5a08
Tom Noonan II <tom@tjnii.com>
2013-09-22 19:57:00 -0500

    Simplified manifest and made nobackups.el optional

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,34 +1,33 @@
-class emacs {
+class emacs (
+  $disable_backups = false,
+  ) {
   case $operatingsystem {
     debian, ubuntu: {
+      $elPath = "/etc/emacs/site-start.d"
+      $elDep  = Package['emacsen-common']
+      
       package { ['emacs23-nox', 'emacsen-common']:
         ensure => installed,
       }
-
-      file { "/etc/emacs/site-start.d/70nobackups.el":
-        ensure  => file,
-        owner   => root,
-        group   => root,
-        mode    => 644,
-        require => Package['emacsen-common'],
-        source  => "puppet:///modules/emacs/70nobackups.el",
-      }
-      
     }
     centos, redhat: {
+      $elPath = "/usr/share/emacs/site-lisp/site-start.d"
+      $elDep  = Package['emacs-common'],
+      
       package { ['emacs-nox', 'emacs-common']:
         ensure => installed,
       }
+    }
+  }
 
-      file { "/usr/share/emacs/site-lisp/site-start.d/nobackups.el":
-        ensure  => file,
-        owner   => root,
-        group   => root,
-        mode    => 644,
-        require => Package['emacs-common'],
-        source  => "puppet:///modules/emacs/70nobackups.el",
-      }
-      
+  if $disable_backups == true {
+    file { "${elPath}/70nobackups.el":
+      ensure  => file,
+      owner   => root,
+      group   => root,
+      mode    => 644,
+      require => $elDep,
+      source  => "puppet:///modules/emacs/70nobackups.el",
     }
   }
   
